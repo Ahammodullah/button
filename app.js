c4e1d11e0d39794bb8c6f21f2ec6a73ac60c5f99
Ahammod <ahammodullah12@gmail.com>
2024-09-23 22:20:17 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+for (var i=0; i<6; i++) {
+    document.querySelectorAll(".myButton")[i].addEventListener("click", function(){
+
+        var text= this.innerHTML;
+        document.querySelector("h1").innerHTML= text + " is clicked"; 
+    
+    
+    
+    });    
+
+
+
+}
+
+// document.querySelectorAll(".myButton")[i].addEventListener("click", function(){
+
+//     var text= this.innerHTML;
+//     document.querySelector("h1").innerHTML= text + " is clicked"; 
+
+
+
+// });
+
+
+// document.querySelectorAll(".myButton")[1].addEventListener("click", function(){
+
+//     var text= this.innerHTML;
+//     document.querySelector("h1").innerHTML= text + " is clicked";
+
+
+
+// });
+
+
+// document.querySelectorAll(".myButton")[2].addEventListener("click", function(){
+
+//     var text= this.innerHTML;
+//     document.querySelector("h1").innerHTML= text + " is clicked"; 
+
+
+
+// });
